38e6eb9a41a609a0bfb56985fbbefe8ff02a0735
masayasviel <i.msya.tiger@gmail.com>
2020-04-16 14:50:07 +0900

    header analysis

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# image_processing
+# 参考
+- [packテンプレート文字列](http://quruli.ivory.ne.jp/document/ruby_1.8/pack_A5C6A5F3A5D7A5ECA1BCA5C8CAB8BBFACEF3.html)
+- [BMP画像のファイル構造](https://algorithm.joho.info/image-processing/bmp-file-data-header/)
+# images
+- http://www.ess.ic.kanagawa-it.ac.jp/app_images_j.html
--- get_image_data.rb
+++ get_image_data.rb
@@ -1,6 +1,18 @@
-file_pass = "./images/napoleon.png"
+file_pass = "./images/Lenna.bmp"
 
-File.open(file_pass, "rb") do |f|
-    p f.gets.chomp
-    p f.gets.chomp
-end
+f = File.open(file_pass, "rb")
+data = f.read
+f.close
+format_type = data[0..1]
+file_size = data[2..5]
+header_size = data[10..13]
+width = data[18..21]
+height = data[22..25]
+color_bit = data[28..29]
+
+puts "format_type: " + format_type
+puts "file_size: " + file_size.unpack("i*")[0].to_s + " [byte]"
+puts "header_size: " + header_size.unpack("i*")[0].to_s + " [byte]"
+puts "image_width: " + width.unpack("i*")[0].to_s + " [px]"
+puts "image_height: " + height.unpack("i*")[0].to_s + " [px]"
+puts "color: " + color_bit.unpack("s*")[0].to_s + " [bit]"
