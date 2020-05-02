16cd98f9f2163fd12803f11ee340e7827e515327
masayasviel <i.msya.tiger@gmail.com>
2020-05-02 13:41:17 +0900

    add grayscale

--- get_image_data.rb
+++ get_image_data.rb
@@ -1,18 +1,18 @@
-file_pass = "./images/Lenna.bmp"
+file_pass = "./monokuro.bmp"
 
 f = File.open(file_pass, "rb")
 data = f.read
 f.close
 format_type = data[0..1]
-file_size = data[2..5]
-header_size = data[10..13]
-width = data[18..21]
-height = data[22..25]
-color_bit = data[28..29]
+file_size = data[2..5].unpack("I*")[0]
+header_size = data[10..13].unpack("I*")[0]
+width = data[18..21].unpack("I*")[0]
+height = data[22..25].unpack("I*")[0]
+color_bit = data[28..29].unpack("C*")[0]
 
 puts "format_type: " + format_type
-puts "file_size: " + file_size.unpack("i*")[0].to_s + " [byte]"
-puts "header_size: " + header_size.unpack("i*")[0].to_s + " [byte]"
-puts "image_width: " + width.unpack("i*")[0].to_s + " [px]"
-puts "image_height: " + height.unpack("i*")[0].to_s + " [px]"
-puts "color: " + color_bit.unpack("s*")[0].to_s + " [bit]"
+puts "file_size: " + file_size.to_s + " [byte]"
+puts "header_size: " + header_size.to_s + " [byte]"
+puts "image_width: " + width.to_s + " [px]"
+puts "image_height: " + height.to_s + " [px]"
+puts "color: " + color_bit.to_s + " [bit]"
---
+++ grayscale.rb
@@ -0,0 +1,42 @@
+file_pass = "./images/Lenna.bmp"
+
+f = File.open(file_pass, "rb")
+data = f.read
+f.close
+format_type = data[0..1]
+file_size = data[2..5].unpack("I*")[0]
+header_size = data[10..13].unpack("I*")[0]
+width = data[18..21].unpack("I*")[0]
+height = data[22..25].unpack("I*")[0]
+color_bit = data[28..29].unpack("C*")[0]
+
+puts "format_type: " + format_type
+puts "file_size: " + file_size.to_s + " [byte]"
+puts "header_size: " + header_size.to_s + " [byte]"
+puts "image_width: " + width.to_s + " [px]"
+puts "image_height: " + height.to_s + " [px]"
+puts "color: " + color_bit.to_s + " [bit]"
+
+monokuro = File.open("monokuro.bmp", "wb")
+
+monokuro.print format_type
+monokuro.print [file_size].pack("i*")
+monokuro.print [0].pack("i*")
+monokuro.print [header_size].pack("i*")
+monokuro.print [40].pack("i*")
+monokuro.print [width].pack("i*")
+monokuro.print [height].pack("i*")
+monokuro.print [1, color_bit].pack("s*")
+monokuro.print [0, 0, 0, 0, 0, 0].pack("i*")
+
+(height*3-1).times do |i|
+    (width).times do |j|
+        monokuro.print [[
+            data[i * width + j + header_size],
+            data[i * width + j + header_size + 1],
+            data[i * width + j + header_size + 2],
+        ].map{|x| x.unpack("C*")[0]}.sum / 3].pack("C*")
+    end
+end
+
+monokuro.close
